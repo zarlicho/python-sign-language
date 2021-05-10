28891b2c5cbae8018db6de5071af7ffc621b0b52
TTV-msVenom <63176268+zarlicho@users.noreply.github.com>
2021-05-10 20:18:12 +0700

    Update final.py

--- openSIBI/final.py
+++ openSIBI/final.py
@@ -3,7 +3,6 @@ import cv2
 import numpy as np
 import csv
 import os
-from pyfirmata import Arduino, util
 
 from sklearn.pipeline import make_pipeline 
 from sklearn.preprocessing import StandardScaler 
@@ -25,12 +24,6 @@ lock2 = 0
 lock3 = 0
 lock4 = 0
 
-board = Arduino('COM6')
-it = util.Iterator(board)
-it.start()
-servo = board.get_pin('d:9:s')
-servo_angle = 0
-servo.write(servo_angle)
 cap = cv2.VideoCapture(0)
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
     
@@ -39,11 +32,7 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
         
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         image.flags.writeable = False        
-        
-        
         results = holistic.process(image)
-        
-        
         image.flags.writeable = True   
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         
@@ -52,21 +41,18 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
                                  mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
                                  )
 
-        #mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-        #                         mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
-        #                         mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
-        #                         )
+        mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                                 mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
+                                 mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
+                                 )
 
         try:
             handR = results.right_hand_landmarks.landmark
             handR_row = list(np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in handR]).flatten())
-            
-            
             row = handR_row
             X = pd.DataFrame([row])
             body_language_class = model.predict(X)[0]
             body_language_prob = model.predict_proba(X)[0]
-            
             xc = body_language_class.split(' ')[0]
 
             if xc == "B":
@@ -78,18 +64,14 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
             elif xc == "A":
                 lock4 += 1
 
-            #print(lock1,lock2,lock3,lock4)
-            angle = 180
             if lock1 > 2 and lock2 >2 and lock3 > 2 and lock4 >2:
                 print("terbuka")
-                servo.write(angle)
                 
             cv2.putText(image, body_language_class.split(' ')[0]
                     ,(90,40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
         except:
             pass    
         cv2.imshow('Raw Webcam Feed', image)
-
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
